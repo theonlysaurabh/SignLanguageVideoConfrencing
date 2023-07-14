4ba8d72f958c95290b6a59482bd1abd993789069
Saurabh Biswas <68153438+theonlysaurabh@users.noreply.github.com>
2023-07-14 23:18:00 +0530

    Delete MajorProject directory

--- MajorProject/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MajorProjectv1.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
-
